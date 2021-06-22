10bec8a7bde0dc695a337f0977d060d86a7793f1
Alvaro Agudelo <alvaro.agudelo@fonyou.com>
2021-06-21 21:27:33 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,7 @@ namespace API
             {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,11 +55,13 @@ namespace API
                 app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection ();
+            app.UseHttpsRedirection();
 
-            app.UseRouting ();
+            app.UseRouting();
 
-            app.UseAuthorization ();
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            app.UseAuthorization();
 
             app.UseEndpoints (endpoints => 
             {
